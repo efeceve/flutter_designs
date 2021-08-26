3f331b9441714ff6cfaa3065da187330a71827fe
Felipe <felipecartagenavasquez@gmail.com>
2021-08-26 19:19:42 -0400

    Scroll screen added

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -5,7 +5,23 @@ class BasicDesignScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [Image(image: AssetImage('assets/landscape.jpg')), Title()],
+        children: [
+          // Banner Image
+          Image(image: AssetImage('assets/landscape.jpg')),
+
+          // Title
+          Title(),
+
+          // Button Section
+          ButtonSection(),
+
+          // Text
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+                'Incididunt adipisicing dolor ut magna irure laboris occaecat. Qui nisi qui elit nostrud aliquip minim reprehenderit quis laboris voluptate. Eu deserunt dolor et enim duis deserunt magna enim deserunt aute enim. Ad tempor velit occaecat dolore pariatur laborum Lorem aliquip est irure aute consectetur irure reprehenderit. Non eiusmod aliquip non velit velit laborum ad dolor culpa eu. Ut quis ullamco anim mollit dolore consectetur officia amet cillum. Sunt cillum dolor labore voluptate nulla voluptate veniam eiusmod tempor ut laborum culpa labore enim.'),
+          )
+        ],
       ),
     );
   }
@@ -43,6 +59,65 @@ class Title extends StatelessWidget {
           Text('41'),
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomButton(icon: Icons.phone, text: 'CALL'),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30),
+          ),
+          CustomButton(
+            icon: Icons.near_me,
+            text: 'ROUTE',
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30),
+          ),
+          CustomButton(
+            icon: Icons.share,
+            text: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          this.icon,
+          color: Colors.lightBlue[400],
+        ),
+        Text(
+          this.text,
+          style: TextStyle(color: Colors.lightBlue[400]),
+        )
+      ],
     );
   }
 }
